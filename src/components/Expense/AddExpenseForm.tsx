aff94f0defe017a0a9a800fd2fc845eb2987ad3e
doriszh0 <doodledodo8@gmail.com>
2024-10-21 16:40:07 -0700

    added tests exercise 4

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -36,6 +36,7 @@ const AddExpenseForm = () => {
             className="form-control"
             id="name"
             value={name}
+            data-testid={"name-input"}
             // HINT: onChange={}
             onChange={(event) => {
               setName(event.target.value)
@@ -50,6 +51,7 @@ const AddExpenseForm = () => {
             className="form-control"
             id="cost"
             value={cost}
+            data-testid={"cost-input"}
             // HINT: onChange={}
             onChange={(event) => {
               setCost(parseInt(event.target.value))
@@ -57,7 +59,7 @@ const AddExpenseForm = () => {
           ></input>
         </div>
         <div className="col-sm">
-          <button type="submit" className="btn btn-primary mt-3">
+          <button type="submit" className="btn btn-primary mt-3" data-testid={"save-button"}>
             Save
           </button>
         </div>
