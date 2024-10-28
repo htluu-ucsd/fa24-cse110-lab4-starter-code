36a4065aa66613c53a567119dadb5e0c9da81697
doriszh0 <doodledodo8@gmail.com>
2024-10-28 15:50:46 -0700

    added change budget

--- client/src/components/Budget/Budget.tsx
+++ client/src/components/Budget/Budget.tsx
@@ -1,12 +1,49 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
+import { fetchBudget } from "../../utils/budget-utils";
 
 const Budget = () => {
   const {budget, setBudget} = useContext(AppContext);
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setBudget(event.currentTarget.value)
+  
+  };
+
+  useEffect(() => {
+    loadBudget();
+    }, []);
+  
+    // Function to load expenses and handle errors
+    const loadBudget = async () => {
+    try {
+      const budgetNum = await fetchBudget();
+      setBudget(budgetNum);
+    } catch (err: any) {
+      console.log(err.message);
+    }
+    };
+
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-      <div>Budget: ${budget}</div>
+      <div>
+      <form onSubmit={(event) => onSubmit(event)}>
+      <label>Budget: </label>
+          <input
+            required
+            type="text"
+            className="form-control"
+            id="budget"
+            value={budget}
+            data-testid={"budget-input"}
+            // HINT: onChange={}
+            onChange={(event) => {
+              setBudget(parseInt(event.target.value))
+            }}
+          ></input>
+          </form>
+          </div>
     </div>
   );
 };
